ab1dd3940eaee3ed621d05c24b15de0e8d55d4fa
Jordan Nieuwhof <jordan.nieuwhof@versapay.com>
2018-01-22 17:03:10 -0500

    Chose a new starting point for the tutorial

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header showLogo={true} />
         <Main />
       </div>
     );
--- src/Header.js
+++ src/Header.js
@@ -3,11 +3,10 @@ import { Link } from 'react-router-dom'
 
 import logo from './logo.svg';
 
-let Header = () => (
+let Header = ({ showLogo }) => (
   <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
+    { showLogo && <img src={logo} className="App-logo" alt="logo" /> }
     <h1 className="App-title">Welcome to React</h1>
-
     <div>
       <ul>
         <li><Link to='/'>Home</Link></li>
